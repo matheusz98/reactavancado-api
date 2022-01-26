2c53c45fc57b4a97394a83cc2a56a1b9b766cd36
Matheus Costa <matheuscastiglia31@gmail.com>
2022-01-26 15:45:40 -0300

    Estrutura inicial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '67a69918cd2203563f7d2054abd6080e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '613094bc-a1c0-4f9e-9309-45da5e4dbe99'
+};
